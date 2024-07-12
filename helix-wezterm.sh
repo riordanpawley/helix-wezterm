c2e248cd434911c061d6a51a26ec37018ec6c641
Riordan Pawley <riordan@dogg.com.au>
2024-07-13 00:17:47 +1000

    fix some lint issues, initial tinkering

--- helix-wezterm.sh
+++ helix-wezterm.sh
@@ -3,8 +3,8 @@
 set -x
 
 status_line=$(wezterm cli get-text | rg -e "(?:NOR\s+|NORMAL|INS\s+|INSERT|SEL\s+|SELECT)\s+[\x{2800}-\x{28FF}]*\s+(\S*)\s[^│]* (\d+):*.*" -o --replace '$1 $2')
-filename=$(echo $status_line | awk '{ print $1}')
-line_number=$(echo $status_line | awk '{ print $2}')
+filename=$(echo "$status_line" | awk '{ print $1}')
+line_number=$(echo "$status_line" | awk '{ print $2}')
 
 split_pane_down() {
   bottom_pane_id=$(wezterm cli get-pane-direction down)
@@ -12,7 +12,7 @@ split_pane_down() {
     bottom_pane_id=$(wezterm cli split-pane)
   fi
 
-  wezterm cli activate-pane-direction --pane-id $bottom_pane_id down
+  wezterm cli activate-pane-direction --pane-id "$bottom_pane_id" down
 
   send_to_bottom_pane="wezterm cli send-text --pane-id $bottom_pane_id --no-paste"
   program=$(wezterm cli list | awk -v pane_id="$bottom_pane_id" '$3==pane_id { print $6 }')
@@ -30,16 +30,8 @@ extension="${filename##*.}"
 case "$1" in
   "blame")
     split_pane_down
+    echo "cd $pwd; git log --pretty=short -u -L $line_number,$line_number:$filename"
     echo "cd $pwd; tig blame $filename +$line_number" | $send_to_bottom_pane
-    ;;
-  "check")
-    split_pane_down
-    case "$extension" in
-      "rs")
-        run_command="cd $pwd/$(echo $filename | sed 's|src/.*$||'); cargo check; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end;"
-        ;;
-    esac
-    echo "$run_command" | $send_to_bottom_pane
     ;;
   "explorer")
     left_pane_id=$(wezterm cli get-pane-direction left)
@@ -49,18 +41,18 @@ case "$1" in
 
     left_program=$(wezterm cli list | awk -v pane_id="$left_pane_id" '$3==pane_id { print $6 }')
     if [ "$left_program" != "br" ]; then
-      echo "br" | wezterm cli send-text --pane-id $left_pane_id --no-paste
+      echo "br" | wezterm cli send-text --pane-id "$left_pane_id" --no-paste
     fi
 
     wezterm cli activate-pane-direction left
     ;;
   "fzf")
     split_pane_down
-    echo "cd $pwd; helix-fzf.sh \$(rg --line-number --column --no-heading --smart-case . | fzf --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window '~3,+{2}+3/2' | awk '{ print \$1 }' | cut -d: -f1,2,3)" | $send_to_bottom_pane
+    echo "clear && cd $pwd; helix-fzf.sh \$(rg --line-number --column --no-heading --smart-case . | fzf --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window '~3,+{2}+3/2' | awk '{ print \$1 }' | cut -d: -f1,2,3)" | $send_to_bottom_pane
     ;;
   "howdoi")
     split_pane_down
-    echo "howdoi -c `pbpaste`" | $send_to_bottom_pane
+    echo "howdoi -c $(pbpaste)" | $send_to_bottom_pane
     ;;
   "lazygit")
     split_pane_down
@@ -72,7 +64,7 @@ case "$1" in
     fi
     ;;
   "open")
-    gh browse $filename:$line_number  
+    gh browse "$filename":"$line_number"  
     ;;
   "run")
     split_pane_down
@@ -90,7 +82,7 @@ case "$1" in
         run_command="racket $filename"
         ;;
       "rs")
-        run_command="cd $pwd/$(echo $filename | sed 's|src/.*$||'); cargo run; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end"
+        run_command="cd $pwd/$(echo "$filename" | sed 's|src/.*$||'); cargo run; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end"
         ;;
       "sh")
         run_command="sh $filename"
@@ -101,7 +93,7 @@ case "$1" in
   "test")
     case "$extension" in
       "go")
-        test_name=$(head -$line_number $filename | tail -1 | sed -n 's/func \([^(]*\).*/\1/p')
+        test_name=$(head -"$line_number" "$filename" | tail -1 | sed -n 's/func \([^(]*\).*/\1/p')
         if [ -n "$test_name" ]; then
           run_command="go test -run=$test_name -v ./$basedir/...; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end;"
         else
@@ -112,11 +104,11 @@ case "$1" in
         run_command="hurl --test --very-verbose --pretty-print --color --variable epoch=$(date +%s) $filename"
         ;;
       "rs")
-        test_name=$(head -$line_number $filename | tail -1 | sed -n 's/^.*fn \([^ ]*\)().*$/\1/p')
-        if [ -n $test_name ]; then
-          run_command="cd $pwd/$(echo $filename | sed 's|src/.*$||'); cargo test $test_name; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end;"
+        test_name=$(head -"$line_number" "$filename" | tail -1 | sed -n 's/^.*fn \([^ ]*\)().*$/\1/p')
+        if [ -n "$test_name" ]; then
+          run_command="cd $pwd/$(echo "$filename" | sed 's|src/.*$||'); cargo test $test_name; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end;"
         else
-          run_command="cd $pwd/$(echo $filename | sed 's|src/.*$||'); cargo test; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end;"
+          run_command="cd $pwd/$(echo "$filename" | sed 's|src/.*$||'); cargo test; if [ \$status = 0 ]; wezterm cli activate-pane-direction up; end;"
         fi
         ;;
     esac
@@ -126,6 +118,6 @@ case "$1" in
     ;;
   "tgpt")
     split_pane_down
-    echo "tgpt '`pbpaste`'" | $send_to_bottom_pane
+    echo "tgpt '$(pbpaste)'" | $send_to_bottom_pane
     ;;
 esac
